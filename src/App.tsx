547f73a099f43c1a9b50209192953f18f1e0b347
Mahdi Navardi <s.mahdinavardi@gmail.com>
2025-11-01 11:26:22 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Home from "./app/components/Home/Index";
 import Index from "./app/Index";
 import { UserProgressContextProvider } from "./store/userProgressContext";
 import { action as authAction } from "./app/actions/auth-action";
 import HotToaster from "./base/Toaster";
+import Home from "./app/components/Home";
 
 function App() {
   const router = createBrowserRouter([
