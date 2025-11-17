be7c2fae91b0aa2b6dd30f8546c5b53a3091607d
Mahdi Navardi <s.mahdinavardi@gmail.com>
2025-11-18 00:15:42 +0330

    Update Forms Catalog
    بخش
    header
    به کامپوننت
    FormsCatalog
    اضافه شد

--- src/app/components/forms-catalog/FormsCatalog.tsx
+++ src/app/components/forms-catalog/FormsCatalog.tsx
@@ -1,5 +1,6 @@
 // import { useLoaderData } from "react-router-dom";
 
+import { toPersianDigits } from "../../../utils/public";
 import FormsCatalogRoadmapStep from "./FormsCatalogRoadmapStep";
 
 const formsCatalog = [
@@ -14,39 +15,49 @@ const formsCatalog = [
 const FormsCatalog = () => {
   // const res = useLoaderData();
   return (
-    <div className="grid grid-cols-2 items-center w-full mt-20">
-      {formsCatalog.map((fc, index) => {
-        const isOdd = (index + 1) % 2 !== 0;
-        return (
-          <>
-            {isOdd && (
-              <>
-                <FormsCatalogRoadmapStep
-                  key={index}
-                  isOdd
-                  counter={index + 1}
-                  imageName={fc.imageName}
-                  title={fc.title}
-                />
-                <div></div>
-              </>
-            )}
-            {!isOdd && (
-              <>
-                <div></div>
-                <FormsCatalogRoadmapStep
-                  key={index}
-                  isOdd={false}
-                  counter={index + 1}
-                  imageName={fc.imageName}
-                  title={fc.title}
-                />
-              </>
-            )}
-          </>
-        );
-      })}
-    </div>
+    <>
+      <div className="w-full mx-auto flex flex-col gap-4 mt-12 text-center text-pretty">
+        <h4>سفر تغذیه‌ای تو از همین‌جا شروع میشه</h4>
+        <p className="xs:px-6 sm:px-16">
+          در یک سفر {toPersianDigits(7)} مرحله‌ای، عادت‌ها و ترجیحاتت رو
+          می‌شناسیم تا در پایان، رژیمی کاملاً مطابق با بدن و سبک زندگی تو
+          بسازیم.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 items-center w-full mt-16">
+        {formsCatalog.map((fc, index) => {
+          const isOdd = (index + 1) % 2 !== 0;
+          return (
+            <>
+              {isOdd && (
+                <>
+                  <FormsCatalogRoadmapStep
+                    key={index}
+                    isOdd
+                    counter={index + 1}
+                    imageName={fc.imageName}
+                    title={fc.title}
+                  />
+                  <div></div>
+                </>
+              )}
+              {!isOdd && (
+                <>
+                  <div></div>
+                  <FormsCatalogRoadmapStep
+                    key={index}
+                    isOdd={false}
+                    counter={index + 1}
+                    imageName={fc.imageName}
+                    title={fc.title}
+                  />
+                </>
+              )}
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
