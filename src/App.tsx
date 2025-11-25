d22c1db484996c6af2c6da65097f632ac8653808
Mahdi Navardi <s.mahdinavardi@gmail.com>
2025-11-26 01:13:50 +0330

    Routing Changes
    ساختار
    routing
    آپدیت شد

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Index from "./app/Index";
 import { UserProgressContextProvider } from "./store/userProgressContext";
-import { action as authAction } from "./app/actions/auth-action";
-import { action as questionnaireAction } from "./app/actions/questionnaire-action";
+import { action as authAction } from "./actions/auth-action";
+import { action as tablematesAction } from "./actions/tablemates-action";
 import HotToaster from "./base/Toaster";
 import Home from "./app/components/home/Index";
 import Diets from "./app/components/diets/Index";
 import News from "./app/components/news/Index";
 import Articles from "./app/components/articles/Index";
 import Podcasts from "./app/components/podcasts/Index";
-import Questionnaire from "./app/components/questionnaire/Index";
-import StartGame from "./app/components/start-game/Index";
+import { loader as gameWorkflowLoader } from "./loaders/game-workflow-loader";
+import Error from "./base/Error";
+import GameWorkflowRoot from "./app/components/game-workflow/Index";
+import GameWorkflow from "./app/components/game-workflow/GameWorkflow";
+import DemographicInformation from "./app/components/game-workflow/demographic-information/Index";
+import Tablemates from "./app/components/game-workflow/tablemates/Index";
+import PreferredFood from "./app/components/game-workflow/preferred-food/Index";
+import PastWeekIntake from "./app/components/game-workflow/past-week-intake/Index";
 
 function App() {
   const router = createBrowserRouter([
@@ -21,16 +27,33 @@ function App() {
       action: authAction,
       children: [
         { index: true, element: <Home /> },
-        { path: "/start-game", element: <StartGame /> },
         {
-          path: "/questionnaire",
-          element: <Questionnaire />,
-          action: questionnaireAction,
+          path: "/game-workflow",
+          element: <GameWorkflowRoot />,
+          children: [
+            {
+              index: true,
+              element: <GameWorkflow />,
+              loader: gameWorkflowLoader,
+            },
+            {
+              path: "demographic-information",
+              element: <DemographicInformation />,
+            },
+            {
+              path: "tablemates",
+              element: <Tablemates />,
+              action: tablematesAction,
+            },
+            { path: "past-week-intake", element: <PastWeekIntake /> },
+            { path: "preferred-food", element: <PreferredFood /> },
+          ],
         },
         { path: "/diets", element: <Diets /> },
         { path: "/news", element: <News /> },
         { path: "/articles", element: <Articles /> },
         { path: "/podcasts", element: <Podcasts /> },
+        { path: "*", element: <Error /> },
       ],
     },
   ]);
