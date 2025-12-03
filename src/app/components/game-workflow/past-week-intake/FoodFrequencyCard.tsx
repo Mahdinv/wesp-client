5bb731edb20aed90e7e45ded9cd946c20e33c4f9
Mahdi Navardi <s.mahdinavardi@gmail.com>
2025-12-03 12:22:37 +0330

    Data Structure Changed
    ساختار دیتا در این مرحله تغییر کرد.
    هنوز کامل نشده و در حال تکمیل شدن است

--- src/app/components/game-workflow/past-week-intake/FoodFrequencyCard.tsx
+++ src/app/components/game-workflow/past-week-intake/FoodFrequencyCard.tsx
@@ -1,9 +1,13 @@
 import React, { memo } from "react";
 
 const FoodFrequencyCard: React.FC<{
-  code: number;
-  title: string;
-  imageName: string;
+  fCard: {
+    id: number;
+    code: number;
+    imageName: string;
+    title: string;
+    properties: { weeklyConsumptionOption: number[] };
+  };
   valuesColor: string;
   value?: number;
   onClick: (value: number, foodFrequencyCode: number) => void;
@@ -12,17 +16,17 @@ const FoodFrequencyCard: React.FC<{
     <div className="flex flex-col items-center gap-2 bg-gray-100 rounded-[20px] shadow-sm py-4 px-0 w-full">
       <div className="flex flex-col items-center gap-3">
         <img
-          src={`/images/food-frequency/${props.imageName}.jpg`}
-          alt={props.imageName}
+          src={`/images/food-frequency/${props.fCard.imageName}.jpg`}
+          alt={props.fCard.imageName}
           loading="lazy"
           className="xs:w-[100px] sm:w-[90px] md:w-[110px] lg:w-[100px] xl:w-[110px] aspect-square rounded-full"
         />
         <label className="xs:text-[14px] sm:text-[12px] md:text-[14px] xl:text-[17px] font-bold text-[#494949] text-nowrap">
-          {props.title}
+          {props.fCard.title}
         </label>
       </div>
       <div className="flex flex-row items-center gap-1 mt-1">
-        {[1, 2, 3, 4].map((v) => (
+        {props.fCard.properties.weeklyConsumptionOption.map((v) => (
           <div
             className="xs:w-7 sm:w-5 md:w-7 lg:w-6 xl:w-7 aspect-square duration-300 rounded-full"
             style={{
@@ -30,7 +34,7 @@ const FoodFrequencyCard: React.FC<{
                 props.value && props.value >= v ? props.valuesColor : "#d1d5db",
             }}
             onClick={() =>
-              props.onClick(props.value !== v ? v : v - 1, props.code)
+              props.onClick(props.value !== v ? v : v - 1, props.fCard.id)
             }
           ></div>
         ))}
--- src/app/components/game-workflow/past-week-intake/Index.tsx
+++ src/app/components/game-workflow/past-week-intake/Index.tsx
@@ -13,14 +13,28 @@ const DUMMYDATA = [
     titleColor: "#AD00DD",
     valuesColor: "#BF73D5",
     foodFrequencyCards: [
-      { code: 108, imageName: "sweet", title: "شیرینی", value: 0 },
       {
+        id: 1,
+        code: 108,
+        imageName: "sweet",
+        title: "شیرینی",
+        categoryId: 1,
+        properties: { weeklyConsumptionOption: [100, 200, 300, 400] },
+      },
+      {
+        id: 2,
         code: 117,
         imageName: "caffeines-chocolates",
         title: "قهوه، چای، شکلات",
-        value: 0,
+        properties: { weeklyConsumptionOption: [40, 80, 100, 160] },
       },
-      { code: 105, imageName: "honey", title: "عسل", value: 0 },
+      {
+        id: 3,
+        code: 105,
+        imageName: "honey",
+        title: "عسل",
+        properties: { weeklyConsumptionOption: [100, 200, 300, 400] },
+      },
     ],
   },
   {
@@ -31,13 +45,26 @@ const DUMMYDATA = [
     titleColor: "#537B01",
     valuesColor: "#9AE009",
     foodFrequencyCards: [
-      { code: 111, imageName: "olive-oil", title: "زیتون", value: 0 },
-      { code: 110, imageName: "herbal-oil", title: "گیاهی", value: 0 },
       {
+        id: 4,
+        code: 111,
+        imageName: "olive-oil",
+        title: "زیتون",
+        properties: { weeklyConsumptionOption: [20, 40, 60, 100] },
+      },
+      {
+        id: 5,
+        code: 110,
+        imageName: "herbal-oil",
+        title: "گیاهی",
+        properties: { weeklyConsumptionOption: [20, 40, 60, 100] },
+      },
+      {
+        id: 6,
         code: 109,
         imageName: "sesame-nuts-oil",
         title: "(کنجد، میوه‌ها، مغزها)",
-        value: 0,
+        properties: { weeklyConsumptionOption: [20, 40, 60, 100] },
       },
     ],
   },
@@ -49,9 +76,27 @@ const DUMMYDATA = [
     titleColor: "#A68500",
     valuesColor: "#FFD83D",
     foodFrequencyCards: [
-      { code: 116, imageName: "potato", title: "سیب‌زمینی", value: 0 },
-      { code: 118, imageName: "rice", title: "برنج", value: 0 },
-      { code: 120, imageName: "bread", title: "نان", value: 0 },
+      {
+        id: 7,
+        code: 116,
+        imageName: "potato",
+        title: "سیب‌زمینی",
+        properties: { weeklyConsumptionOption: [400, 800, 1200, 1600] },
+      },
+      {
+        id: 8,
+        code: 118,
+        imageName: "rice",
+        title: "برنج",
+        properties: { weeklyConsumptionOption: [400, 800, 1200, 1600] },
+      },
+      {
+        id: 9,
+        code: 120,
+        imageName: "bread",
+        title: "نان",
+        properties: { weeklyConsumptionOption: [400, 800, 1200, 1600] },
+      },
     ],
   },
   {
@@ -62,11 +107,41 @@ const DUMMYDATA = [
     titleColor: "#DC4F4F",
     valuesColor: "#FF3D3D",
     foodFrequencyCards: [
-      { code: 106, imageName: "fish", title: "ماهی", value: 0 },
-      { code: 103, imageName: "meat", title: "گوشت قرمز", value: 0 },
-      { code: 104, imageName: "chicken", title: "گوشت مرغ", value: 0 },
-      { code: 101, imageName: "egg", title: "تخم‌مرغ", value: 0 },
-      { code: 102, imageName: "dairy", title: "لبنیات", value: 0 },
+      {
+        id: 10,
+        code: 106,
+        imageName: "fish",
+        title: "ماهی",
+        properties: { weeklyConsumptionOption: [200, 400, 600, 800] },
+      },
+      {
+        id: 11,
+        code: 103,
+        imageName: "meat",
+        title: "گوشت قرمز",
+        properties: { weeklyConsumptionOption: [130, 260, 390, 520] },
+      },
+      {
+        id: 12,
+        code: 104,
+        imageName: "chicken",
+        title: "گوشت مرغ",
+        properties: { weeklyConsumptionOption: [160, 320, 480, 700] },
+      },
+      {
+        id: 13,
+        code: 101,
+        imageName: "egg",
+        title: "تخم‌مرغ",
+        properties: { weeklyConsumptionOption: [240, 480, 720, 1000] },
+      },
+      {
+        id: 14,
+        code: 102,
+        imageName: "dairy",
+        title: "لبنیات",
+        properties: { weeklyConsumptionOption: [500, 1000, 1500, 2000] },
+      },
     ],
   },
   {
@@ -77,10 +152,27 @@ const DUMMYDATA = [
     titleColor: "#3D9769",
     valuesColor: "#0EA758",
     foodFrequencyCards: [
-      { code: 113, imageName: "vegetable", title: "سبزیجات", value: 0 },
-      { code: 16, imageName: "dates", title: "خرما", value: 0 },
-      { code: 17, imageName: "olive", title: "زیتون", value: 0 },
-      { code: 112, imageName: "fruits", title: "میوه‌ها", value: 0 },
+      {
+        id: 15,
+        code: 113,
+        imageName: "vegetable",
+        title: "سبزیجات",
+        properties: { weeklyConsumptionOption: [150, 300, 450, 600] },
+      },
+      {
+        id: 16,
+        code: 107,
+        imageName: "olive-dates",
+        title: "خرما و زیتون",
+        properties: { weeklyConsumptionOption: [50, 100, 200, 400] },
+      },
+      {
+        id: 17,
+        code: 112,
+        imageName: "fruits",
+        title: "میوه‌ها",
+        properties: { weeklyConsumptionOption: [300, 600, 900, 1200] },
+      },
     ],
   },
   {
@@ -91,29 +183,87 @@ const DUMMYDATA = [
     titleColor: "#B17E2F",
     valuesColor: "#CC7D03",
     foodFrequencyCards: [
-      { code: 114, imageName: "nuts", title: "آجیل‌ها", value: 0 },
-      { code: 115, imageName: "beans", title: "حبوبات", value: 0 },
-      { code: 119, imageName: "cereals", title: "غلات", value: 0 },
+      {
+        id: 18,
+        code: 114,
+        imageName: "nuts",
+        title: "آجیل‌ها",
+        properties: { weeklyConsumptionOption: [60, 120, 180, 240] },
+      },
+      {
+        id: 19,
+        code: 115,
+        imageName: "beans",
+        title: "حبوبات",
+        properties: { weeklyConsumptionOption: [300, 600, 900, 1200] },
+      },
+      {
+        id: 20,
+        code: 119,
+        imageName: "cereals",
+        title: "غلات",
+        properties: { weeklyConsumptionOption: [300, 600, 900, 1200] },
+      },
     ],
   },
 ];
 
+const finalData = [
+  { foodGroupId: 1, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 2, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 3, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 4, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 5, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 6, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 7, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 8, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 9, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 10, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 11, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 12, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 13, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 14, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 15, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 16, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 17, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 18, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 19, userId: 1, value: 0, percentUsage: 0 },
+  { foodGroupId: 20, userId: 1, value: 0, percentUsage: 0 },
+];
+
 const PastWeekIntake = () => {
-  const [foodFrequencyCards, setFoodFrequencyCards] = useState(DUMMYDATA);
+  const [foodFrequencyCards, setFoodFrequencyCards] = useState<
+    | {
+        foodGroupId: number;
+        userId: number;
+        value: number;
+        percentUsage: number;
+      }[]
+    | null
+  >(finalData);
 
   const handleSetFoodFrequencyValue = useCallback(
-    function handleSetFoodFrequencyValue(value: number, code: number) {
+    function handleSetFoodFrequencyValue(value: number, id: number) {
+      // setFoodFrequencyCards((prev) =>
+      //   prev.map((ff) => {
+      //     const index = ff.foodFrequencyCards.fi zndIndex(
+      //       (ffc) => ffc.code === code
+      //     );
+      //     if (index === -1) return ff;
+      //     const updatedCards = [...ff.foodFrequencyCards];
+      //     updatedCards[index] = { ...updatedCards[index], value };
+      //     return { ...ff, foodFrequencyCards: updatedCards };
+      //   })
+      // );
       setFoodFrequencyCards((prev) =>
-        prev.map((ff) => {
-          const index = ff.foodFrequencyCards.findIndex(
-            (ffc) => ffc.code === code
-          );
-          if (index === -1) return ff;
-
-          const updatedCards = [...ff.foodFrequencyCards];
-          updatedCards[index] = { ...updatedCards[index], value };
-
-          return { ...ff, foodFrequencyCards: updatedCards };
+        (prev || []).map((ff) => {
+          if (ff.foodGroupId === id) {
+            return {
+              ...ff,
+              value,
+            };
+          }
+          return ff;
         })
       );
     },
@@ -126,12 +276,12 @@ const PastWeekIntake = () => {
         headingTitle="در هفته گذشته از هر گروه غذایی چند وعده استفاده کردی؟"
         title="با دایره‌ها مشخص کن که مصرفت از هر گروه غذایی چقدر بوده."
       />
-      <div className="xs:w-full sm:w-9/12 md:w-10/12 lg:w-9/12 xl:w-7/12 2xl:w-6/12 xs:px-4 sm:mx-auto mt-7">
+      <div className="xs:w-full sm:w-[90%] md:w-10/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12 sm:mx-auto xs:px-2 mt-7">
         <FoodFrequencyHelpBar />
       </div>
       <div className="flex xs:flex-col lg:flex-row items-center xs:gap-6 md:gap-4 w-full mt-12 md:px-10 2xl:px-28">
-        <div className="flex flex-col gap-4 xs:w-full lg:w-8/12 xs:px-4 sm:px-6 md:px-0">
-          {foodFrequencyCards.map((card) => (
+        <div className="flex flex-col gap-4 xs:w-full lg:w-7/12 xs:px-4 sm:px-6 md:px-0">
+          {DUMMYDATA.map((card) => (
             <FoodGroupCard
               key={card.name}
               title={card.title}
@@ -139,17 +289,19 @@ const PastWeekIntake = () => {
               titleColor={card.titleColor}
               iconColor={card.iconColor}
             >
-              <div className="grid xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 items-center justify-center xs:gap-3 md:gap-6">
+              <div className="grid xs:grid-cols-2 sm:grid-cols-3 items-center justify-center xs:gap-3 md:gap-6">
                 {card.foodFrequencyCards.map((fCard) => (
                   <FoodFrequencyCard
                     key={fCard.code}
-                    code={fCard.code}
-                    imageName={fCard.imageName}
-                    title={fCard.title}
+                    fCard={fCard}
                     valuesColor={card.valuesColor}
-                    value={fCard.value}
-                    onClick={(value, foodFrequencyCode) =>
-                      handleSetFoodFrequencyValue(value, foodFrequencyCode)
+                    value={
+                      (foodFrequencyCards || []).find(
+                        (fd) => fd.foodGroupId === fCard.id
+                      )?.value
+                    }
+                    onClick={(value, foodFrequencyId) =>
+                      handleSetFoodFrequencyValue(value, foodFrequencyId)
                     }
                   />
                 ))}
@@ -157,7 +309,9 @@ const PastWeekIntake = () => {
             </FoodGroupCard>
           ))}
         </div>
-        <div className="flex-grow bg-blue-300 self-start">2</div>
+        <div className="flex-grow self-start">
+          <div className="w-96 aspect-square bg-red-400 rounded-full mx-auto"></div>
+        </div>
       </div>
     </>
   );
