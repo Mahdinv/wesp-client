f71c85338bff54fadda57bf129a5e44e5e4ccefb
Mahdi Navardi <s.mahdinavardi@gmail.com>
2025-12-22 07:45:23 +0330

    Disable Design Added
    طراحی کارت های قفل و غیرفعال کامل و کدنویسی شد

--- src/app/components/game-workflow/GameWorkflow.tsx
+++ src/app/components/game-workflow/GameWorkflow.tsx
@@ -1,20 +1,28 @@
-// import { useLoaderData } from "react-router-dom";
 import Intro from "../../../base/Intro";
 import { toPersianDigits } from "../../../utils/public";
 import GameWorkflowRoadmapStep from "./GameWorkflowRoadmapStep";
 
 const formsCatalog = [
-  { id: 1, imageName: "demographic-information", title: "پرسشنامه اولیه" },
-  { id: 2, imageName: "tablemates", title: "همسفره‌ها" },
-  { id: 3, imageName: "past-week-intake", title: "رژیم فعلی" },
-  { id: 4, imageName: "preferred-food", title: "علاقه تو" },
-  { id: 5, imageName: "free-shopping", title: "خرید آزاد" },
-  { id: 6, imageName: "limited-shopping", title: "خرید محدود" },
-  { id: 7, imageName: "social-alignment", title: "همسویی با جامعه" },
+  {
+    id: 1,
+    imageName: "demographic-information",
+    title: "پرسشنامه اولیه",
+    disable: false,
+  },
+  { id: 2, imageName: "tablemates", title: "همسفره‌ها", disable: true },
+  { id: 3, imageName: "past-week-intake", title: "رژیم فعلی", disable: true },
+  { id: 4, imageName: "preferred-food", title: "علاقه تو", disable: true },
+  { id: 5, imageName: "free-shopping", title: "خرید آزاد", disable: true },
+  { id: 6, imageName: "limited-shopping", title: "خرید محدود", disable: true },
+  {
+    id: 7,
+    imageName: "social-alignment",
+    title: "همسویی با جامعه",
+    disable: true,
+  },
 ];
 
 const GameWorkflow = () => {
-  // const res = useLoaderData();
   return (
     <>
       <Intro
@@ -23,7 +31,7 @@ const GameWorkflow = () => {
           می‌شناسیم تا در پایان، رژیمی کاملاً مطابق با بدن و سبک زندگی تو
           بسازیم.`}
       />
-      <div className="grid grid-cols-2 items-center w-full xs:mt-7 lg:mt-16">
+      <div className="grid grid-cols-2 items-center w-full xxs:mt-7 lg:mt-16">
         {formsCatalog.map((fc, index) => {
           const isOdd = (index + 1) % 2 !== 0;
           return (
@@ -33,20 +41,21 @@ const GameWorkflow = () => {
               counter={index + 1}
               imageName={fc.imageName}
               title={fc.title}
+              disable={fc.disable}
             />
           );
         })}
       </div>
-      <div className="xs:w-full md:w-[81%] lg:w-[83%] xl:w-[75%] 2xl:[70%] xs:px-6 sm:px-10 md:mx-auto mt-10">
-        <div className="relative flex flex-col items-center justify-center bg-yellow-100 border-4 border-yellow-500 rounded-[39px] [box-shadow:0_27px_78px_0_#FFDF5F96] py-8 xs:gap-4 lg:gap-8">
-          <div className="absolute left-0 top-0 -translate-x-1/2 -translate-y-1/2 bg-green-500 rounded-full xs:w-9 sm:w-14 md:w-16 lg:w-20 xl:w-24 aspect-square flex items-center justify-center xs:text-base lg:text-5xl text-white">
-            <div className="font-rokh xs:text-xs sm:text-lg md:text-xl lg:text-2xl xl:text-5xl xs:translate-y-1 lg:translate-y-2">
+      <div className="xxs:w-full md:w-[81%] lg:w-[83%] xl:w-[75%] 2xl:[70%] xxs:px-6 sm:px-10 md:mx-auto mt-10">
+        <div className="relative flex flex-col items-center justify-center bg-yellow-100 border-4 border-yellow-500 rounded-[39px] [box-shadow:0_27px_78px_0_#FFDF5F96] py-8 xxs:gap-4 lg:gap-8">
+          <div className="absolute left-0 top-0 -translate-x-1/2 -translate-y-1/2 bg-green-500 rounded-full xxs:w-9 sm:w-14 md:w-16 lg:w-20 xl:w-24 aspect-square flex items-center justify-center xxs:text-base lg:text-5xl text-white">
+            <div className="font-rokh xxs:text-xs sm:text-lg md:text-xl lg:text-2xl xl:text-5xl xxs:translate-y-1 lg:translate-y-2">
               {toPersianDigits(formsCatalog.length + 1)}
             </div>
           </div>
 
           <img
-            className="xs:w-[126px] sm:w-[140px] md:w-[200px] lg:w-[250px] 2xl:w-[309px] aspect-square xs:rounded-xl rounded-[48px] border border-black object-cover"
+            className="xxs:w-[126px] sm:w-[140px] md:w-[200px] lg:w-[250px] 2xl:w-[309px] aspect-square xxs:rounded-xl rounded-[48px] border border-black object-cover"
             src="/images/forms-catalog-roadmap-step/step-eight.jpg"
             alt=""
           />
@@ -54,7 +63,7 @@ const GameWorkflow = () => {
           <h2 className="text-[#007A55]">رژیم غذایی پایدار شخصی‌سازی‌شده</h2>
         </div>
       </div>
-      <div className="xs:w-full xs:px-4 xl:w-[70%] xl:mx-auto xs:mt-12 xl:mt-16">
+      <div className="xxs:w-full xxs:px-4 xl:w-[70%] xl:mx-auto xxs:mt-12 xl:mt-16">
         <h3 className="!font-peyda text-center text-[#001F17] opacity-80">
           فقط {toPersianDigits(15)} دقیقه تا دستیابی به رژیم غذایی شخصی‌سازی‌شده
         </h3>
--- src/app/components/game-workflow/GameWorkflowRoadmapStep.tsx
+++ src/app/components/game-workflow/GameWorkflowRoadmapStep.tsx
@@ -1,32 +1,35 @@
 import type React from "react";
 import { toPersianDigits } from "../../../utils/public";
+import Button from "../../../base/inputs/Button";
+import { MdLockOutline } from "react-icons/md";
 
 const GameWorkflowRoadmapStep: React.FC<{
   isOdd: boolean;
   counter: number;
   imageName: string;
   title: string;
+  disable: boolean;
 }> = (props) => {
   return (
     <div className="relative my-9">
       <div
-        className={`absolute xs:w-6 sm:w-7 lg:w-10 aspect-square bg-green-500 rounded-full z-10 ${
+        className={`absolute xxs:w-6 sm:w-7 lg:w-10 aspect-square bg-green-500 rounded-full z-10 ${
           props.isOdd
             ? "left-0 top-1/2 -translate-x-1/2 -translate-y-1/2"
             : "right-0 -bottom-[18%] translate-x-1/2 translate-y-1/2"
         }`}
       ></div>
       <div
-        className={`absolute w-1/2 xs:h-[2px] sm:h-[3px] md:h-1 bg-green-500 ${
+        className={`absolute w-1/2 xxs:h-[2px] sm:h-[3px] md:h-1 bg-green-500 ${
           props.isOdd
             ? "left-0 top-1/2 -translate-y-1/2"
             : "right-0 -bottom-[18%]"
         }`}
       ></div>
       <div
-        className={`absolute xs:w-[2px] lg:w-1 bg-[#003526] ${
+        className={`absolute xxs:w-[2px] lg:w-1 bg-[#003526] ${
           props.counter !== 1
-            ? "xs:h-[160%] sm:h-[150%] top-1/2"
+            ? "xxs:h-[160%] sm:h-[150%] top-1/2"
             : "h-1/2 bottom-0"
         } ${
           props.isOdd ? "left-0 -translate-x-1/2" : "right-0 translate-x-1/2"
@@ -35,29 +38,62 @@ const GameWorkflowRoadmapStep: React.FC<{
       <div
         className={`relative ${
           !props.isOdd ? "bottom-0 translate-y-2/3" : undefined
-        } flex flex-col items-center xs:gap-2 sm:gap-4 lg:gap-6 xs:w-9/12 sm:w-8/12 md:w-5/12 lg:w-6/12 xl:w-2/5 xs:rounded-lg lg:rounded-2xl xs:shadow-lg lg:shadow-2xl bg-white xs:border-2 md:border-4 border-green-500 mx-auto xs:py-2 lg:py-6`}
+        } flex flex-col items-center xxs:gap-1 md:gap-2 xxs:w-9/12 sm:w-8/12 md:w-5/12 lg:w-6/12 xl:w-2/5 xxs:rounded-lg lg:rounded-2xl xxs:shadow-lg lg:shadow-2xl bg-back-page xxs:border-2 md:border-4 ${
+          props.disable ? "border-[#73CE95]" : "border-primary"
+        } mx-auto xxs:py-2 lg:py-6 px-2`}
       >
         <div
-          className={`bg-green-500 rounded-full absolute xs:w-9 sm:w-14 md:w-16 lg:w-20 xl:w-24 aspect-square flex items-center justify-center xs:text-base lg:text-5xl text-white ${
+          className={`absolute top-0 w-full h-full xxs:rounded-md lg:rounded-xl bg-white ${
+            props.disable ? "opacity-50" : undefined
+          }`}
+        ></div>
+        <div
+          className={`${
+            props.disable ? "bg-[#CBDED7]" : "bg-green-500"
+          } rounded-full absolute xxs:w-9 xs:w-12 sm:w-14 md:w-16 lg:w-20 xl:w-24 aspect-square flex items-center justify-center xxs:text-base lg:text-5xl text-white ${
             props.isOdd
               ? "top-0 right-0 translate-x-1/2 -translate-y-1/2"
               : "top-0 left-0 -translate-x-1/2 -translate-y-1/2"
           }`}
         >
-          <div className="font-rokh xs:text-xs sm:text-lg md:text-xl lg:text-2xl xl:text-5xl xs:translate-y-1 lg:translate-y-2">
-            {toPersianDigits(props.counter)}
+          <div
+            className={`${
+              !props.disable
+                ? "font-rokh xxs:text-xs xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-5xl xxs:translate-y-1 lg:translate-y-2"
+                : undefined
+            }`}
+          >
+            {!props.disable ? (
+              toPersianDigits(props.counter)
+            ) : (
+              <MdLockOutline className="text-[#737B76] xxs:w-6 xxs:h-6 sm:w-8 sm:h-8 md:w-9 md:h-9 lg:w-12 lg:h-12 xl:w-14 xl:h-14 2xl:w-16 2xl:h-16" />
+            )}
           </div>
         </div>
-        <div className="xs:w-20 sm:w-28 md:w-32 lg:w-40 xl:w-44 aspect-square overflow-hidden rounded-full border border-[#001F17]">
+        <div
+          className={`xxs:w-20 sm:w-28 md:w-32 lg:w-40 xl:w-44 aspect-square overflow-hidden rounded-full border border-[#001F17] z-10 ${
+            props.disable ? "opacity-50" : undefined
+          }`}
+        >
           <img
             src={`/images/forms-catalog-roadmap-step/${props.imageName}.jpg`}
-            alt=""
+            alt={props.imageName}
             className="object-cover scale-125"
           />
         </div>
-        <h4 className="text-[#007A55] text-center !font-peyda">
+        <h4
+          className={`text-[#007A55] text-center !font-peyda relative z-10 ${
+            props.disable ? "opacity-50" : undefined
+          }`}
+        >
           {props.title}
         </h4>
+        {!props.disable && (
+          <Button
+            classes="btn btn-primary xxs:w-full sm:w-2/3 md:w-full lg:w-2/3 xxs:!py-[2px] md:!py-1 lg:!py-2 xl:!py-1 xxs:!text-xxs sm:!text-xs lg:!text-sm xl:!text-base 2xl:!text-lg !rounded-full relative z-10"
+            title="شروع کن"
+          />
+        )}
       </div>
     </div>
   );
